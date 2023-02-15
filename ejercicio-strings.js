d6c1a363a3040579822de526a7e815d752ae66a4
AlejoJato <115959829+AlejoJato@users.noreply.github.com>
2023-02-15 10:59:59 -0300

    Ejercicio 4 del curso de javascript
    En este ejercicio he tenido que crear diferentes cadenas de texto utilizando algunos de los metodos mas conocidos y comunmente vistos

---
+++ ejercicio-strings.js
@@ -0,0 +1,19 @@
+let nombre = "Alejo";
+
+let apellido = "Jato";
+
+let estudiante = `${nombre} ${apellido}`;
+
+let estudianteMayus = estudiante.toUpperCase();
+
+let estudianteMinus = estudiante.toLowerCase();
+
+let letras = estudiante.length;
+
+let primera_letra = nombre.charAt(0);
+
+let ultima_letra = apellido[3];
+
+let sin_espacios = estudiante.replace(" ", "");
+
+let bool = estudiante.includes(`${nombre}`);
