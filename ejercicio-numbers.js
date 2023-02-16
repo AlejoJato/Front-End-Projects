25834c1351daee63974b3188dfb7200b3538e453
AlejoJato <115959829+AlejoJato@users.noreply.github.com>
2023-02-15 22:04:14 -0300

    Ejercicio 05 del curso de Javascript
    En esta ocasion la consigna fue crear diferentes variables con metodos exclusivos de los numbers

---
+++ ejercicio-numbers.js
@@ -0,0 +1,11 @@
+let alutra_cm = 170;
+
+let alutra_mts = 1.69;
+
+let peso_kgs = 72.450;
+
+let altura_redondeo = alutra_mts.toPrecision(2);
+
+let peso_redondeo = peso_kgs.toFixed();
+
+let igualdad = (Number.MAX_VALUE + 1) === Number.MAX_VALUE;
